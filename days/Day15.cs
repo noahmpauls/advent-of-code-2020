ba6681eca40f911433165e33e248f729ca1a99e4
Noah Pauls <noahmpauls@gmail.com>
2020-12-15 22:27:16 -0700

    completed day 15

---
+++ days/Day15.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace days
+{
+    public class Day15
+    {
+        //######################################################################
+        // Part 1 + 2 Solutions
+        //######################################################################
+
+        public static int Part1()
+        {
+            const string path = Helpers.inputPath + @"\day15\input.txt";
+            IList<int> inputs = ProcessInputFile(path);
+
+            const int turns = 2020;
+            return MemoryGame(inputs, turns);
+        }
+
+        public static int Part2()
+        {
+            const string path = Helpers.inputPath + @"\day15\input.txt";
+            IList<int> inputs = ProcessInputFile(path);
+
+            const int turns = 30000000;
+            return MemoryGame(inputs, turns);
+        }
+
+        //######################################################################
+        // Methods
+        //######################################################################
+
+        public static IList<int> ProcessInputFile(string path)
+        {
+            return Helpers.ProcessInputFile(path, line => int.Parse(line));
+        }
+
+        public static int MemoryGame(IList<int> start, int turns)
+        {
+            IDictionary<int, int> recentIndices = new Dictionary<int, int>();
+            for (int i = 0; i < start.Count - 1; i++)
+            {
+                recentIndices.Add(start[i], i);
+            }
+
+            int mostRecent = start.Last();
+            int mostRecentIndex = start.Count - 1;
+
+            while (mostRecentIndex < turns-1)
+            {
+                int newRecent = 0;
+                if (recentIndices.ContainsKey(mostRecent))
+                {
+                    newRecent = mostRecentIndex - recentIndices[mostRecent];
+                }
+                recentIndices[mostRecent] = mostRecentIndex;
+                mostRecent = newRecent;
+                mostRecentIndex++;
+            }
+
+            return mostRecent;
+        }
+
+    }
+
+    //##########################################################################
+    // Helper Classes
+    //##########################################################################
+
+    // public class Day/* day */Result : DayResult<void, void> { }
+
+}
+
--- days/Program.cs
+++ days/Program.cs
@@ -11,17 +11,13 @@ namespace days
             Console.WriteLine("Begin");
 
             Console.WriteLine("Part 1 ----------------------------------------");
-            Console.WriteLine(Day14.Part1());
+            Console.WriteLine(Day15.Part1());
             Console.WriteLine("");
             Console.WriteLine("Part 2 ----------------------------------------");
-            Console.WriteLine(Day14.Part2());
+            Console.WriteLine(Day15.Part2());
 
             Console.WriteLine("");
             Console.WriteLine("Tests  ----------------------------------------");
-            Day14.Mask mask = new Day14.Mask("X1X01X01X000110001011X010X0111X01001");
-            Console.WriteLine(mask.Raw);
-            Console.WriteLine(Convert.ToString((long)mask.Zeros, 2));
-            Console.WriteLine(Convert.ToString((long)mask.Ones, 2));
 
             Console.WriteLine("End");
         }
